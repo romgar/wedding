ce98edf86ccec77a441ff1bce6ecbc69eea3de57
romgar <romain.garrigues.cs@gmail.com>
2019-01-12 10:34:10 +0000

    - Move css and js file to the same folder - Justify text for A&R section - Increase h1/h2/h3/h4/h5 font size - Remove padding between sections

--- js/main.js
+++ js/main.js
@@ -23,7 +23,7 @@ $("a.smooth-scroll").click(function(event) {
       event.preventDefault();
       $("html, body").animate(
         {
-          scrollTop: target.offset().top
+          scrollTop: target.offset().top - 50
         },
         1000,
         function() {
@@ -43,51 +43,6 @@ $("a.smooth-scroll").click(function(event) {
     }
   }
 });
-
-// Photo Filter
-var activeFilter = "all";
-
-$(".ww-filter-button").on("click", function(e) {
-  // remove btn-primary from all buttons first
-  $(".ww-filter-button").removeClass("btn-primary");
-  $(".ww-filter-button").addClass("btn-outline-primary");
-
-  // add btn-primary to active button
-  var button = $(this);
-  button.removeClass("btn-outline-primary");
-  button.addClass("btn-primary");
-  filterItems(button.data("filter"));
-  e.preventDefault();
-});
-
-function filterItems(filter) {
-  if (filter === activeFilter) {
-    return;
-  }
-
-  activeFilter = filter;
-  $(".ww-gallery .card").each(function() {
-    var card = $(this);
-    var groups = card.data("groups");
-    var show = false;
-    if (filter === "all") {
-      show = true;
-    } else {
-      for (var i = 0; i < groups.length; i++) {
-        if (groups[i] === filter) {
-          show = true;
-        }
-      }
-    }
-    // hide everything first
-    card.fadeOut(400);
-    setTimeout(function() {
-      if (show && !card.is(":visible")) {
-        card.fadeIn(400);
-      }
-    }, 500);
-  });
-}
 
 // Light Box
 $(document).on("click", '[data-toggle="lightbox"]', function(event) {
