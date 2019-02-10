51ecbb650e9c839c9b40269bbec2ba6d4605954f
romgar <romain.garrigues.cs@gmail.com>
2019-02-10 22:37:45 +0000

    Removing the obscure month - 1 calculation in the simplyCountdown logic

--- js/simplyCountdown.js
+++ js/simplyCountdown.js
@@ -175,7 +175,7 @@
 
         targetTmpDate = new Date(
             parameters.year,
-            parameters.month - 1,
+            parameters.month,
             parameters.day,
             parameters.hours,
             parameters.minutes,
